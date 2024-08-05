824839728d1c8fd608adcb379e2cb3823b8b8cab
Neb Yelsaw <github@yelsaw.com>
2024-08-04 20:47:43 -0500

    add refresh and countdown using bubble tea

--- main.go
+++ main.go
@@ -5,10 +5,20 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/pquerna/otp/totp"
 )
+
+type messageData struct {
+	secret    string
+	period    int
+	code      string
+	countdown int
+	ticker    *time.Ticker
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -16,8 +26,8 @@ func main() {
 	}
 
 	otpUrl := os.Args[1]
-
 	u, err := url.Parse(otpUrl)
+
 	if err != nil {
 		log.Fatalf("Unable to parse 'totp' url: %v", err)
 	}
@@ -27,10 +37,84 @@ func main() {
 		log.Fatalf("Missing 'secret' from url")
 	}
 
-	otpCode, err := totp.GenerateCode(secret, time.Now())
-	if err != nil {
-		log.Fatalf("Unable to retrieve code: %v", err)
+	periodParam := u.Query().Get("period")
+	period := 30
+	if periodParam != "" {
+		period, err = strconv.Atoi(periodParam)
+		if err != nil {
+			log.Fatalf("Invalid 'period' parameter: %v", err)
+		}
 	}
 
-	fmt.Println("Code: ", otpCode)
+	program := tea.NewProgram(initData(secret, period))
+	if err := program.Start(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func initData(secret string, period int) messageData {
+	return messageData{
+		secret:    secret,
+		period:    period,
+		countdown: period,
+		ticker:    time.NewTicker(time.Second),
+	}
+}
+
+func getCode(secret string) tea.Cmd {
+	return func() tea.Msg {
+		code, err := totp.GenerateCode(secret, time.Now())
+		if err != nil {
+			return errMsg{err}
+		}
+		return codeMsg{code}
+	}
+}
+
+type codeMsg struct {
+	code string
+}
+
+type errMsg struct {
+	err error
+}
+
+func tickCmd(ticker *time.Ticker) tea.Cmd {
+	return func() tea.Msg {
+		return <-ticker.C
+	}
+}
+
+// Bubble Tea: Init()
+func (m messageData) Init() tea.Cmd {
+	return tea.Batch(tickCmd(m.ticker), getCode(m.secret))
+}
+
+// Bubble Tea: Update()
+func (m messageData) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "q" {
+			return m, tea.Quit
+		}
+	case codeMsg:
+		m.code = msg.code
+		m.countdown = m.period
+		return m, tickCmd(m.ticker)
+	case time.Time:
+		m.countdown--
+		if m.countdown <= 0 {
+			return m, getCode(m.secret)
+		}
+		return m, tickCmd(m.ticker)
+	case errMsg:
+		log.Fatalf("Unable to retrieve code: %v", msg.err)
+	}
+	return m, nil
+}
+
+// Bubble Tea: View()
+func (m messageData) View() string {
+	return fmt.Sprintf("\n\nToken: %s\nExpires in %d seconds\n\nPress q to quit\n\n\n", m.code, m.countdown)
 }
